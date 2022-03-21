917ac1e7a6185bf0b35b4a1519c523d1ed726a53
Tanmay <tanmayguha16@gmail.com>
2022-03-21 14:06:05 +0530

    comments added

--- chapter5/chapter_5_1.py
+++ chapter5/chapter_5_1.py
@@ -1,4 +1,10 @@
+#Topic = dictionary
+
+
+#Project takes any inventory
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+#function just prints the item using a for loop and gives the total item
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
@@ -6,6 +12,7 @@ def displayInventory(inventory):
         print(str(v) + ' ' + k)
         item_total += v
     print("Total number of items: " + str(item_total))
+
 displayInventory(stuff)
 
 #Solution was already there
--- chapter5/chapter_5_2.py
+++ chapter5/chapter_5_2.py
@@ -1,4 +1,6 @@
+#topic: dictionary
 
+# Fuction takes dictionary input and prints it with a for loop
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
@@ -7,11 +9,15 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
+# Function's first paraeter takes dictionary and second takes list
 def addToInventory(inventory, addedItems):
+    # loops over the list
     for i in addedItems:
+        # if item not in dict add the item in keys
         if i not in inventory:
             inventory[i] = 1
         else:
+            #if item is there just add the value once
             inventory[i]+=1;
 
 inv = {'gold coin': 42, 'rope': 1}
